ac5d3cd8cbf8e8bd17d837240e1e935fa001e17a
Deiver Carrascal <ingendeiver@gmail.com>
2021-07-07 01:09:15 -0500

    .gitignore fixed

--- templates/base-tamplate/dist/src/dtos/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-exports.UserDTO = void 0;
-/* eslint-disable import/prefer-default-export */
-var UserDTO_1 = __importDefault(require("./UserDTO"));
-exports.UserDTO = UserDTO_1["default"];
--- templates/base-tamplate/dist/src/services/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-exports.__esModule = true;
-exports.UserService = void 0;
-var UserService_1 = __importDefault(require("./UserService"));
-exports.UserService = UserService_1["default"];
